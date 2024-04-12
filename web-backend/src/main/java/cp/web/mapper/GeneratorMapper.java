c8b414cf01caec456d06624e42d5910a4a3a5fa1
cp <cp@itcast.cn>
2024-04-12 14:11:19 +0800

    进行了后端项目初始化，并对前端项目初始化，完成了用户注册页面、管理页面、主页的搜素进行开发。

---
+++ web-backend/src/main/java/cp/web/mapper/GeneratorMapper.java
@@ -0,0 +1,18 @@
+package cp.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import cp.web.model.entity.Generator;
+
+/**
+ * @author cp
+ * @description 针对表【generator(代码生成器)】的数据库操作Mapper
+ * @createDate 2024-04-09 20:24:50
+ * @Entity cp.web.model.entity.Generator
+ */
+public interface GeneratorMapper extends BaseMapper<Generator> {
+
+}
+
+
+
+
